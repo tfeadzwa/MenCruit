c3f32c2ec18bada083876f91cd69b685f9fd996e
tfeadzwa <tfadzwa02@gmail.com>
2023-10-23 23:29:30 +0200

    add login page

--- App/js/home.js
+++ App/js/home.js
@@ -1,98 +1,100 @@
+// Format JavaScript code using Prettier and ESLint
+// Source: [1] [2]
+
+// Define functions for scroll events
+function onScrollEvent() {
+  const header = document.querySelector(".header");
+
+  // header.addEventListener("touchmove", function () {
+  //     header.setAttribute("data-scroll", "true");
+  // });
+
+  window.addEventListener("scroll", function () {
+    if (window.scrollY === 0) {
+      header.removeAttribute("data-scroll");
+    } else {
+      header.setAttribute("data-scroll", "true");
+    }
+  });
+}
+
+function onScrollEvent2() {
+  window.onscroll = () => {
+    return scrollFunction();
+  };
+
+  function scrollFunction() {
+    const header = document.querySelector("header");
+    if (document.body.scrollTop > 5 || document.documentElement.scrollTop > 5) {
+      header.classList.add("onscroll");
+    } else {
+      header.classList.remove("onscroll");
+    }
+  }
+}
+
+// Define function for displaying menu items
+function displayMenuItems() {
+  let headerLinks = document.querySelectorAll(".header__links a");
+  let menuItems = document.querySelectorAll(".menu .header__menu");
+  let menuLinks = document.querySelector(".menu");
+
+  for (let i = 0; i < menuItems.length; i++) {
+    headerLinks[i].addEventListener("focus", function () {
+      menuLinks.classList.add("menu-overlay");
+      menuItems[i].style.visibility = "visible";
+    });
+
+    headerLinks[i].addEventListener("blur", function () {
+      menuLinks.classList.remove("menu-overlay");
+      menuItems[i].style.visibility = "hidden";
+    });
+  }
+}
+
+// Define function for horizontal scrolling
+function horizontalScroll() {
+  const scrollImages = document.querySelector(".hrtools__cards-container");
+  const scrollLength = scrollImages.scrollWidth - scrollImages.clientWidth;
+  const leftButton = document.querySelector(".hrtools__left-arrow");
+  const rightButton = document.querySelector(".hrtools__right-arrow");
+
+  function checkScroll() {
+    const currentScroll = scrollImages.scrollLeft;
+    if (currentScroll === 0) {
+      leftButton.setAttribute("disabled", "true");
+      rightButton.removeAttribute("disabled");
+    } else if (currentScroll === scrollLength) {
+      rightButton.setAttribute("disabled", "true");
+      leftButton.removeAttribute("disabled");
+    } else {
+      leftButton.removeAttribute("disabled");
+      rightButton.removeAttribute("disabled");
+    }
+  }
+
+  scrollImages.addEventListener("scroll", checkScroll);
+  window.addEventListener("resize", checkScroll);
+
+  function leftScroll() {
+    scrollImages.scrollBy({
+      left: -320,
+      behavior: "smooth",
+    });
+  }
+
+  function rightScroll() {
+    scrollImages.scrollBy({
+      left: 320,
+      behavior: "smooth",
+    });
+  }
+
+  leftButton.addEventListener("click", leftScroll);
+  rightButton.addEventListener("click", rightScroll);
+}
+
+// Call the functions
 onScrollEvent();
 displayMenuItems();
 horizontalScroll();
-
-function onScrollEvent() {
-    const header = document.querySelector(".header");
-
-    // header.addEventListener("touchmove", function () {
-    //     header.setAttribute("data-scroll", "true");
-    // });
-
-    window.addEventListener("scroll", function () {
-        if (window.scrollY === 0) {
-            header.removeAttribute("data-scroll");
-        } else {
-            header.setAttribute("data-scroll", "true");
-        }
-    });
-}
-
-// same with onScrollEvent , make sure to add class 'onscroll' to the style sheet
-function onScrollEvent2() {
-    window.onscroll = () => {
-        return scrollFunction();
-    };
-
-    function scrollFunction() {
-        const header = document.querySelector("header");
-        if (document.body.scrollTop > 5 || document.documentElement.scrollTop > 5) {
-            header.classList.add('onscroll');
-        } else {
-            header.classList.remove('onscroll');
-        }
-    }
-}
-
-//? display menu items for each link item on mouse hover & mouse out
-function displayMenuItems() {
-    let headerLinks = document.querySelectorAll(".header__links a");
-    let menuItems = document.querySelectorAll(".menu .header__menu");
-    let menuLinks = document.querySelector(".menu");
-
-    for (let i = 0; i < menuItems.length; i++) {
-
-        headerLinks[i].addEventListener("mouseover", function () {
-            menuLinks.classList.add("menu-overlay");
-            menuItems[i].style.visibility = "visible";
-        });
-
-        headerLinks[i].addEventListener("mouseout", function () {
-            menuLinks.classList.remove("menu-overlay");
-            menuItems[i].style.visibility = "hidden";
-        });
-    }
-
-}
-
-
-function horizontalScroll() {
-    const scrollImages = document.querySelector(".hrtools__cards-container");
-    const scrollLength = scrollImages.scrollWidth - scrollImages.clientWidth;
-    const leftButton = document.querySelector(".hrtools__left-arrow");
-    const rightButton = document.querySelector(".hrtools__right-arrow");
-
-    function checkScroll() {
-        const currentScroll = scrollImages.scrollLeft;
-        if (currentScroll === 0) {
-            leftButton.setAttribute("disabled", "true");
-            rightButton.removeAttribute("disabled");
-        } else if (currentScroll === scrollLength) {
-            rightButton.setAttribute("disabled", "true");
-            leftButton.removeAttribute("disabled");
-        } else {
-            leftButton.removeAttribute("disabled");
-            rightButton.removeAttribute("disabled");
-        }
-    }
-
-    scrollImages.addEventListener("scroll", checkScroll);
-    window.addEventListener("resize", checkScroll);
-
-    function leftScroll() {
-        scrollImages.scrollBy({
-            left: -320,
-            behavior: "smooth"
-        });
-    }
-
-    function rightScroll() {
-        scrollImages.scrollBy({
-            left: 320,
-            behavior: "smooth"
-        });
-    }
-
-    leftButton.addEventListener("click", leftScroll);
-    rightButton.addEventListener("click", rightScroll);
-}
---
+++ App/js/login.js
@@ -0,0 +1,13 @@
+// This is the JavaScript code for the login feature
+
+// This function logs in the user with Google using Firebase Authentication
+function loginWithGoogle() {}
+
+// This function logs in the user with email and password using Firebase Authentication
+function loginWithEmail(event) {}
+
+// This function stores the authentication tokens in local storage using Firebase Authentication
+function storeTokens() {}
+
+// This function validates the user inputs and handles any errors using Firebase Authentication
+function validateInputs() {}
