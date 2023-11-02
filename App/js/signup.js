276813ef52de1fe1ad5f92a5ef39f36e0ea4f147
tfeadzwa <tfadzwa02@gmail.com>
2023-11-02 23:07:37 +0200

    add signup page design

---
+++ App/js/signup.js
@@ -0,0 +1,27 @@
+//ToDo: use dataset
+
+const tabsBtn = document.querySelectorAll(".tab__button");
+const tabsContainer = document.querySelectorAll(".tab__container");
+const btnNext = document.getElementById("next-button");
+let prevBtnContext = tabsBtn[0];
+
+const tabImages = {};
+
+tabsBtn.forEach((tab, index) => {
+  tab.addEventListener("click", () => {
+    btnNext.style.display = tab.id.includes("employee") ? "none" : "block";
+    // convert NodeList array like object to array object and get the index
+    const prevIndex = [...tabsBtn].indexOf(prevBtnContext);
+
+    // hide the previous tab container and display current one
+    tabsContainer[prevIndex].style.display = "none";
+    tabsContainer[index].style.display = "flex";
+    prevBtnContext = tab;
+  });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  let tab = document.getElementById("tab-business");
+  tab.click();
+  tab.focus();
+});
