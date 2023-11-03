5222c1c33261e9817fd6c004a0f1d8ef056c18e6
tfeadzwa <tfadzwa02@gmail.com>
2023-11-03 23:20:26 +0200

    add side illustration

--- App/js/signup.js
+++ App/js/signup.js
@@ -8,7 +8,10 @@ let prevBtnContext = tabsBtn[0];
 const tabImages = {};
 
 tabsBtn.forEach((tab, index) => {
-  tab.addEventListener("click", () => {
+  tab.addEventListener("click", (e) => {
+    tab.style.cssText =
+      "border-color: var(--royal-blue); background-color: hsl(219, 91%, 96%); color: black;";
+    // hide the next button
     btnNext.style.display = tab.id.includes("employee") ? "none" : "block";
     // convert NodeList array like object to array object and get the index
     const prevIndex = [...tabsBtn].indexOf(prevBtnContext);
