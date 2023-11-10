446e89e73c767466d85726621fd3e6118c4d810b
tfeadzwa <tfadzwa02@gmail.com>
2023-11-10 23:51:48 +0200

    feat: Add select field in signup form page

---
+++ App/js/utils/sideIllustration.mjs
@@ -0,0 +1,31 @@
+export const sideContent = [
+  {
+    title: "Business",
+    color: "var(--white)",
+    bgColor: "rgb(21, 53, 122)",
+    h2Content: "Hire internationally with complete confidence",
+    h4Content:
+      "We'll handle your worldwide compliance, payroll, and benefits, so you can fast-track your international expansion.",
+    imgContent: "/app/assets/icons/business-icon.svg",
+  },
+
+  {
+    title: "Contractor",
+    color: "var(--dark-cerulean)",
+    bgColor: "rgb(153, 202, 249)",
+    h2Content: "Work anywhere and get paid in any currency",
+    h4Content:
+      "Work compliantly from 150 countries, automate your invoicing, and get paid in any currency, including Crypto.",
+    imgContent: "/app/assets/icons/contractor-icon.svg",
+  },
+
+  {
+    title: "Employee",
+    color: "var(--white)",
+    bgColor: "rgb(189, 128, 255)",
+    h2Content: "One place to manage everything",
+    h4Content:
+      "Review your contract, access payslips and manage benefits, time off and more in just one place.",
+    imgContent: "/app/assets/icons/employee-icon.svg",
+  },
+];
