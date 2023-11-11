9ca9f105762a250715ead900657ad60913794329
tfeadzwa <tfadzwa02@gmail.com>
2023-11-11 23:41:52 +0200

    fix: update header component to a fixed position

--- App/js/signup-form.js
+++ App/js/signup-form.js
@@ -3,6 +3,8 @@ import { sideContent } from "./utils/sideIllustration.mjs";
 // get the index of selected tab
 const index = JSON.parse(localStorage.getItem("index"));
 
+addSideIllustration();
+
 function addSideIllustration() {
   // change color, h2, h4, and image content
 
@@ -24,4 +26,35 @@ function addSideIllustration() {
   imgContent.src = ill.imgContent;
 }
 
-addSideIllustration();
+// check selected option from dropdown items
+const optionsList = document.querySelectorAll(".dropdown-menu li");
+
+optionsList.forEach((option) => {
+  function handler(e) {
+    checkSelectedOption();
+  }
+
+  option.addEventListener("keyup", handler);
+  option.addEventListener("click", handler);
+});
+
+function checkSelectedOption() {
+  const selected = document.querySelector(".selected-value").textContent.trim();
+  const explainField = document.querySelector(".field-explain");
+  // const sourceField = document.querySelector(".field-source");
+
+  // display Others field
+  if (selected === "Other") {
+    explainField.setAttribute("is-visible", "true");
+  } else {
+    explainField.setAttribute("is-visible", "false");
+  }
+
+  // ToDo: display Referred field
+  if (selected === "Referred/Recommended") {
+    // const sourceField = document.querySelector(".field-source");
+    // sourceField.setAttribute("is-visible", "true");
+  } else {
+    // sourceField.setAttribute("is-visible", "false");
+  }
+}
