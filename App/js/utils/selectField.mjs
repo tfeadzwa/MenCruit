2b4685ad765f856f466b1a79e0ad0d3d2fe70ba5
tfeadzwa <tfadzwa02@gmail.com>
2023-11-13 15:05:58 +0200

    Feat(signup): Display source options on click

--- App/js/utils/selectField.mjs
+++ App/js/utils/selectField.mjs
@@ -1,3 +1,57 @@
+const fieldSelect = document.querySelectorAll(".field-select");
+
+let selectButton;
+let selectedValue;
+let optionsList;
+let selected;
+
+fieldSelect.forEach((select) => {
+  select.addEventListener("click", (e) => {
+    const sel = select.className.split(" ")[1];
+    console.log(sel);
+
+    selectButton = document.querySelector(`.${sel} .select-button`);
+    selectedValue = document.querySelector(`.${sel} .selected-value`);
+    optionsList = document.querySelectorAll(`.${sel} .dropdown-menu li`);
+    selected = select;
+
+    onSelectButtonClick();
+    onOptionsListClick();
+  });
+});
+
+function onSelectButtonClick() {
+  selectButton.addEventListener("click", (e) => {
+    selected.classList.toggle("field-select__active");
+    selectButton.setAttribute(
+      "aria-expanded",
+      selectButton.getAttribute("aria-expanded") === true ? "false" : "true"
+    );
+  });
+}
+
+function onOptionsListClick() {
+  optionsList.forEach((option) => {
+    function handler(e) {
+      // Click Events
+      if (e.type === "click" && e.clientX !== 0 && e.clientY !== 0) {
+        selectedValue.textContent = this.textContent;
+      }
+      // Key Events
+      if (e.key === "Enter") {
+        selectedValue.textContent = this.textContent;
+      }
+      selected.classList.remove("field-select__active");
+
+      e.removeEventListener("click");
+    }
+
+    option.addEventListener("keyup", handler);
+    option.addEventListener("click", handler);
+  });
+}
+
+/*
 const fieldSelect = document.querySelector(".field-select");
 const selectButton = document.querySelector(".select-button");
 
@@ -28,3 +82,4 @@ optionsList.forEach((option) => {
   option.addEventListener("keyup", handler);
   option.addEventListener("click", handler);
 });
+*/
