ed5d57e8aab754cce5fa8176c09c0a647e88624a
tfeadzwa <tfadzwa02@gmail.com>
2023-12-27 00:17:21 +0200

    feat: Add getstarted component to the home page

---
+++ src/components/GetStarted/GetStarted.jsx
@@ -0,0 +1,16 @@
+const GetStarted = () => {
+  return (
+    <section className="getstarted">
+      <div className="container container--pall">
+        <h2>Get started with MenCruit today</h2>
+        <p>
+          Join the other 500+ companies managing their global teams with
+          MenCruit
+        </p>
+        <button>Request A Demo</button>
+      </div>
+    </section>
+  );
+};
+
+export default GetStarted;
--- src/components/index.jsx
+++ src/components/index.jsx
@@ -3,5 +3,5 @@ export { default as Hero } from "./Hero/Hero";
 export { default as Platform } from "./Platform/OnePlatform";
 export { default as Hrtools } from "./Hrtools/Hrtools";
 export { default as Benefits } from "./Benefits/Benefits";
-export { default as GetStarted } from "./GetStarted";
+export { default as GetStarted } from "./GetStarted/GetStarted";
 export { default as Footer } from "./Footer";
